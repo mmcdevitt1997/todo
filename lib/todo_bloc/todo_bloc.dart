d033ff0a923159a216061cadd8c4ff4d58c0924f
Matthew McDevitt <mmcdevitt1997@gmail.com>
2024-06-27 16:37:53 -0500

    starting up agian

--- lib/todo_bloc/todo_bloc.dart
+++ lib/todo_bloc/todo_bloc.dart
@@ -15,16 +15,89 @@ class TodoBloc extends HydratedBloc<TodoEvent, TodoState> {
     on<AlterTodo>(_onAlterTodo);
   }
 
-  
-  @override
-  TodoState? fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+  void _onStarted(
+    TodoStarted event,
+    Emitter<TodoState> emit,
+  ) {
+    if (state.status == TodoStatus.success) return;
+    emit(state.copyWith(todos: state.todos, status: TodoStatus.success));
   }
-  
-  @override
-  Map<String, dynamic>? toJson(TodoState state) {
-    // TODO: implement toJson
-    throw UnimplementedError();
+
+  void _onAddTodo(
+    AddTodo event,
+    Emitter<TodoState> emit,
+  ) {
+    emit(state.copyWith(status: TodoStatus.loading));
+    try {
+      List<Todo> temp = [];
+      temp.addAll(state.todos);
+      temp.insert(0, event.todo);
+      emit(state.copyWith(todos: temp, status: TodoStatus.success));
+    } catch (e) {
+      emit(state.copyWith(status: TodoStatus.error));
+    }
   }
+
+	void _onRemoveTodo(
+		RemoveTodo event,
+		Emitter<TodoState> emit,
+	) {
+		emit(
+			state.copyWith(
+				status: TodoStatus.loading
+			)
+		);
+		try {
+			state.todos.remove(event.todo);
+			emit(
+				state.copyWith(
+					todos: state.todos,
+					status: TodoStatus.success
+				)
+			);
+		} catch (e) {
+		  emit(
+				state.copyWith(
+					status: TodoStatus.error
+				)
+			);
+		}
+	}
+
+
+	void _onAlterTodo(
+		AlterTodo event,
+		Emitter<TodoState> emit,
+	) {
+		emit(
+			state.copyWith(
+				status: TodoStatus.loading
+			)
+		);
+		try {
+			state.todos[event.index].isDone = !state.todos[event.index].isDone;
+			emit(
+				state.copyWith(
+					todos: state.todos,
+					status: TodoStatus.success
+				)
+			);
+		} catch (e) {
+		  emit(
+				state.copyWith(
+					status: TodoStatus.error
+				)
+			);
+		}
+	}
+	
+	 @override
+	 TodoState? fromJson(Map<String, dynamic> json) {
+		return TodoState.fromJson(json);
+	 }
+	
+	 @override
+	 Map<String, dynamic>? toJson(TodoState state) {
+		 return state.toJson();
+	 }
 }
